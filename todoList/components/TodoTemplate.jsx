f98f641e5942620da01e4958124ac20271defaf2
ksy7533 <ksy7533@gmail.com>
2019-11-18 21:24:53 +0900

    css 관련 로더 설정

--- todoList/components/TodoTemplate.jsx
+++ todoList/components/TodoTemplate.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import './TodoTemplate.scss';
 
 const TodoTemplate = ({ children }) => {
-  console.log(children);
   return (
-    <div className="TodoTemplatee">
+    <div className="TodoTemplate">
       <div className="app-title">일정 관리</div>
       <div className="content">{children}</div>
     </div>
