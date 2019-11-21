362d4350e09c48bb8b73567507d0125384f454ae
ksy7533 <ksy7533@gmail.com>
2019-11-21 23:12:51 +0900

    라우터 query 실습

--- router_test/About.jsx
+++ router_test/About.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import qs from "qs";
 
-const About = () => {
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true // 문자열 맨앞의 ?를 생략한다
+  });
+  const showDetail = query.detail === "true";
   return (
     <div>
       <h1>소개d</h1>
       <p>기초 소개</p>
+      {showDetail && <p>detail값이 true!</p>}
     </div>
   );
 };
