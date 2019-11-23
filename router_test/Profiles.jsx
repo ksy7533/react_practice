79c90abb1f9f300c5c0f0ba569f73d5fe4f74a28
ksy7533 <ksy7533@gmail.com>
2019-11-23 22:23:18 +0900

    NavLink 콤포넌트로 해당하는 경로로 이동하는 경우 특정 스타일을 적용할수있도록한다

--- router_test/Profiles.jsx
+++ router_test/Profiles.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { NavLink, Route } from "react-router-dom";
 import Profile from "./Profile";
 
 const Profiles = () => {
+  const activeStyle = {
+    color: "red"
+  };
+
   return (
     <div>
       <h3>사용자 목록</h3>
-      <Link to="/profiles/test">test</Link>
-      <Link to="/profiles/test02">test02</Link>
+      <NavLink activeStyle={activeStyle} to="/profiles/test">
+        test
+      </NavLink>
+      <NavLink activeStyle={activeStyle} to="/profiles/test02">
+        test02
+      </NavLink>
       <Route
         path="/profiles"
         exact
