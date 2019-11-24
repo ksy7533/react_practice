5fec2ec68e9b4530b13d2365fd247b73487e7628
ksy7533 <ksy7533@gmail.com>
2019-11-25 00:38:20 +0900

    불필요한 코드 정리

--- api_test/src/components/Categories.jsx
+++ api_test/src/components/Categories.jsx
@@ -70,7 +70,7 @@ const Category = styled(NavLink)`
   }
 `;
 
-const Categories = ({ category, onSelect }) => {
+const Categories = () => {
   return (
     <CategoriesBlock>
       {categories.map(c => (
