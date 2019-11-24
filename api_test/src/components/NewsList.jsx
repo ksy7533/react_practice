495dfb8b30ccd01b383bba8b44822f35fc603efb
ksy7533 <ksy7533@gmail.com>
2019-11-24 16:03:30 +0900

    newsList api 호출

--- api_test/src/components/NewsList.jsx
+++ api_test/src/components/NewsList.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import axios from "axios";
 import NewsItem from "./NewsItem";
 
 const NewsListBlock = styled.div`
@@ -15,19 +16,40 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const sampleArticle = {
-  title: "제목",
-  description: "내용",
-  url: "http://google.com",
-  urlToImage: "https://via.placeholder.com/160"
-};
-
 const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(
+          "https://newsapi.org/v2/top-headlines?country=kr&apiKey=b9da5dc4df844d2b9586ae7dd28ebf39"
+        );
+        setArticles(response.data.articles);
+      } catch (error) {
+        console.log(error);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <NewsListBlock>isLoading....</NewsListBlock>;
+  }
+
+  if (!articles) {
+    // 첫번째 렌더링시 articles값이 null이므로 처리해줘야한다
+    return null;
+  }
+
   return (
     <NewsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {articles.map(article => (
+        <NewsItem article={article} key={article.url} />
+      ))}
     </NewsListBlock>
   );
 };
