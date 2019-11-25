f73b3ecfc59cd4bc92ad71ced32e2c233d560e8a
ksy7533 <ksy7533@gmail.com>
2019-11-25 16:07:48 +0900

    redux 설정 및 devTool설정

--- redux_test/Client.jsx
+++ redux_test/Client.jsx
@@ -1,7 +1,19 @@
 import { hot } from "react-hot-loader/root";
 import React from "react";
 import ReactDom from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./App";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
 const Hot = hot(App);
 
-ReactDom.render(<Hot />, document.querySelector("#root"));
+ReactDom.render(
+  <Provider store={store}>
+    <Hot />
+  </Provider>,
+  document.querySelector("#root")
+);
